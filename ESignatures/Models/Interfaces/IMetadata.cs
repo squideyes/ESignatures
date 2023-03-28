0970b933ad805a5fc3e824a6fd0773d95f616a67
Louis S. Berman <louis@squideyes.com>
2023-03-28 13:57:02 -0400

    More refactoring

---
+++ ESignatures/Models/Interfaces/IMetadata.cs
@@ -0,0 +1,7 @@
+﻿namespace SquidEyes.ESignatures;
+
+public interface IMetadata<T>
+    where T : IMetadata<T>, new()
+{
+    T Parse(string value);
+}
--- SharedModels/Entities/WebHookError.cs
+++ SharedModels/Entities/WebHookError.cs
@@ -4,8 +4,8 @@ public class WebHookError : IWebHook<WebHookError>
 {
     public Guid ContractId { get; set; }
     public Metadata? Metadata { get; set; }
-    public string? ErrorCode { get; set; }
     public string? Message { get; set; }
+    public string? ErrorCode { get; set; }
 
     public WebHookKind WebHookKind => WebHookKind.WebHookError;
 }
